b03b90af2d764cde9faa086c0055fe6ea7861a24
Jacob <JRTSNJHS@live.com>
2020-10-05 13:26:28 -0700

    Apply track

---
+++ AddingMachine.java
@@ -0,0 +1,26 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () { // Initializes the adding machine
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () { // Retrieves the total
+    return 0;
+  }
+  
+  public void add (int value) { // Adds value to the total
+  }
+
+  public void subtract (int value) { // Subtracts value from total
+  }
+
+  public String toString () { // Prints the total
+    return "";
+  }
+
+  public void clear() { // Clears total
+  }
+}
